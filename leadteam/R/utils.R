f5deeba41a4ee75f835d8efc2fd0c96253fa2f5d
Nabil A <nabilabdu@gatech.edu>
2015-09-23 09:48:03 -0400

    removed another hindrance

--- leadteam/R/utils.R
+++ leadteam/R/utils.R
@@ -2,7 +2,7 @@
 #' Removes Helper Columns
 #'
 #' @importFrom dplyr select
-#' @importFrom magrittr `%>%`
+#' @importFrom magrittr %>%
 #'
 clean_cols <- function(df) {
   df %>% select(-ends_with("_row"))
@@ -11,7 +11,7 @@ clean_cols <- function(df) {
 #' Removes spaces in column names
 #'
 #' @importFrom stringr str_replace_all
-#' @importFrom magrittr `%>%`
+#' @importFrom magrittr %>%
 #'
 rm_space_names <- function(df) {
   set_names(., str_replace_all(names(.), " ", ""))
